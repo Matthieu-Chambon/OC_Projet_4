94a625f22a340677856d5ddac2acbc7e42baa5bf
Matthieu <matthieu.chambon44@gmail.com>
2025-02-25 19:43:34 +0100

    suppression de la vue rapportView

--- views/__init__.py
+++ views/__init__.py
@@ -1,6 +1,5 @@
 from .menuView import MenuView
-from .rapportView import Rapport
 from .tournamentView import TournamentView
 from .userView import UserView
 
-__all__ = ['MenuView', 'Rapport', 'TournamentView', 'UserView']
+__all__ = ['MenuView', 'TournamentView', 'UserView']
